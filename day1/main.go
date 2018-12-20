58b65c4921e081419390144017484f566cf0f11a
Geert Gerritsen <ggerritsen@ebay.com>
2018-12-20 15:29:43 +0100

    Modified for part 2

--- day1/main.go
+++ day1/main.go
@@ -8,25 +8,69 @@ import (
 	"strconv"
 )
 
-
 func main() {
-	freq  := int64(0)
-	fmt.Printf("Starting with frequency %d...\n", freq)
+	f := int64(0)
+	seenFrequencies := map[int64]bool{}
+	seenFrequencies[f] = true
+	fmt.Printf("Starting with frequency %d...\n", f)
+
+	i := 0
+	var firstDoubleFreq *int64 = nil
+	for firstDoubleFreq == nil {
+		i = i + 1
+		//fmt.Printf("Start loop %d with frequency %d\n", i, f)
+
+		freq, d, err := loop(f, seenFrequencies)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f = freq
+		//fmt.Printf("End loop %d with frequency %d.\n", i, f)
+
+		if d != nil {
+			firstDoubleFreq = d
+		}
+	}
+
+	fmt.Printf("First frequency reached twice: %d (in %d loops).\n", *firstDoubleFreq, i)
+}
+
+
+func loop(freq int64, seenFreqs map[int64]bool) (int64, *int64, error) {
+	found := false
+	var firstDoubleFreq int64 = 0
 
 	f, err := os.Open("input.txt")
 	if err != nil {
-		log.Fatal(err)
+		return 0, nil, err
 	}
+	defer f.Close()
 
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		freq, err = updateFrequency(freq, s.Text())
 		if err != nil {
-			log.Fatal(err)
+			return 0, nil, err
+		}
+
+		if found {
+			continue
+		}
+
+		if seenFreqs[freq] {
+			firstDoubleFreq = freq
+			found = true
+		} else {
+			seenFreqs[freq] = true
 		}
 	}
 
-	fmt.Printf("End with frequency %d.\n", freq)
+	if !found {
+		return freq, nil, nil
+	}
+
+	return freq, &firstDoubleFreq, nil
 }
 
 func updateFrequency(f int64, s string) (int64, error) {
